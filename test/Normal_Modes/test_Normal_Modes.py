d0ed6dfdc3036c217443c7f79c19acf76dccb597
ekinesme <ekinesme@hotmail.com>
2025-03-12 18:50:40 +0100

    test again

--- test/Normal_Modes/test_Normal_Modes.py
+++ test/Normal_Modes/test_Normal_Modes.py
@@ -25,12 +25,6 @@ def run_vibrant(binary_path:str, input_file:str, omp_threads:int=1):
     return calc.returncode, calc.stdout
 
     
-def parse_normal_freq_from_file(fname:str):
-    """ parse the generated normal mode frequencies """
-    data = np.loadtxt(fname)
-    x = data[:]
-    return x
-
 def parse_normal_displ_from_file(fname:str):
     """ parse the generated normal mode displacements """
     data = np.loadtxt(fname)
@@ -39,7 +33,6 @@ def parse_normal_displ_from_file(fname:str):
     z = data[:, 2]
     return x, y, z
 
-
 def test_Normal_Modes(vibrant_binary_path):
     """ run test case normal modes """
     # run vibrant calculation and test for successfull exit
@@ -47,21 +40,16 @@ def test_Normal_Modes(vibrant_binary_path):
     assert returncode == 0
     
     # file names
-    reference1 = "output/normal_mode_freq.txt"
-    test1 = "normal_mode_freq.txt"
     
-    reference2 = "output/normal_mode_displ.txt" 
-    test2 = "normal_mode_displ.txt"
+    reference = "output/normal_mode_displ.txt" 
+    test = "normal_mode_displ.txt"
 
     # parse filenames
-    x_ref1 = parse_normal_freq_from_file(reference1)
-    x_test1 = parse_normal_freq_from_file(test1)
 
-    x_ref2, y_ref2, z_ref2 = parse_normal_displ_from_file(reference2)
-    x_test2, y_test2, z_test2 = parse_normal_displ_from_file(test2)
+    x_ref, y_ref, z_ref = parse_normal_displ_from_file(reference)
+    x_test, y_test, z_test = parse_normal_displ_from_file(test)
     
     # compare test against reference
-    assert np.allclose(x_ref1, x_test1, atol=1e-8)
-    assert np.allclose(x_ref2, x_test2, atol=1e-8)
-    assert np.allclose(y_ref2, y_test2, atol=1e-8)
-    assert np.allclose(z_ref2, z_test2, atol=1e-8)
+    assert np.allclose(x_ref, x_test, atol=1e-8)
+    assert np.allclose(y_ref, y_test, atol=1e-8)
+    assert np.allclose(z_ref, z_test, atol=1e-8)
