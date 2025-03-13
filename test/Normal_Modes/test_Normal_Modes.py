2cfa7cd7c8f6f814510e219a5a7db3d9008bf0b0
ekinesme <ekinesme@hotmail.com>
2025-03-13 14:16:15 +0100

    improve the test

--- test/Normal_Modes/test_Normal_Modes.py
+++ test/Normal_Modes/test_Normal_Modes.py
@@ -24,6 +24,11 @@ def run_vibrant(binary_path:str, input_file:str, omp_threads:int=1):
     )
     return calc.returncode, calc.stdout
 
+def parse_normal_freq_from_file(fname:str):
+    """ parse the generated normal mode frequencies """
+    data = np.loadtxt(fname)
+    x = data[:]
+    return x
     
 def parse_normal_displ_from_file(fname:str):
     """ parse the generated normal mode displacements """
@@ -39,17 +44,22 @@ def test_Normal_Modes(vibrant_binary_path):
     returncode, stdout = run_vibrant(vibrant_binary_path, "input.txt")
     assert returncode == 0
     
-    # file names
+    # file names    
+    reference1 = "output/normal_mode_freq.txt"
+    test1 = "normal_mode_freq.txt"
     
-    reference = "output/normal_mode_displ.txt" 
-    test = "normal_mode_displ.txt"
+    reference2 = "output/normal_mode_displ.txt" 
+    test2 = "normal_mode_displ.txt"
 
     # parse filenames
+    x_ref1 = parse_normal_freq_from_file(reference1)
+    x_test1 = parse_normal_freq_from_file(test1)
 
-    x_ref, y_ref, z_ref = parse_normal_displ_from_file(reference)
-    x_test, y_test, z_test = parse_normal_displ_from_file(test)
+    x_ref2, y_ref2, z_ref2 = parse_normal_displ_from_file(reference2)
+    x_test2, y_test2, z_test2 = parse_normal_displ_from_file(test2)
     
     # compare test against reference
-    assert np.allclose(np.abs(x_ref), np.abs(x_test), atol=1e-8)
-    assert np.allclose(np.abs(y_ref), np.abs(y_test), atol=1e-8)
-    assert np.allclose(np.abs(z_ref), np.abs(z_test), atol=1e-8)
+    assert np.allclose(np.abs(x_ref1), np.abs(x_test1), atol=1e-8)
+    assert np.allclose(np.abs(x_ref2), np.abs(x_test2), atol=1e-8)
+    assert np.allclose(np.abs(y_ref2), np.abs(y_test2), atol=1e-8)
+    assert np.allclose(np.abs(z_ref2), np.abs(z_test2), atol=1e-8)
