2b4cb9ff4b95b9f18053e42af8fa80d4c923cc77
moritzleucke <moritzleucke.privat@gmail.com>
2025-10-13 12:59:49 +0200

    fix website again

--- cmake/website.cmake
+++ cmake/website.cmake
@@ -7,10 +7,19 @@ set(SPHINX_CACHE_DIR "${CMAKE_CURRENT_BINARY_DIR}/_doctrees")
 # HTML output directory
 set(SPHINX_HTML_DIR "${CMAKE_CURRENT_BINARY_DIR}/html")
 
+#add_custom_target(docs ALL
+#    ${SPHINX_EXECUTABLE}
+#        -b html
+#        -d "${SPHINX_CACHE_DIR}"
+#        "${CMAKE_CURRENT_SOURCE_DIR}/docs/"
+#        "${SPHINX_HTML_DIR}"
+#    COMMENT "Building HTML documentation with Sphinx")
 add_custom_target(docs ALL
-    ${SPHINX_EXECUTABLE}
+    COMMAND ${SPHINX_EXECUTABLE}
         -b html
+        -c "${CMAKE_CURRENT_SOURCE_DIR}/docs"
         -d "${SPHINX_CACHE_DIR}"
-        "${CMAKE_CURRENT_SOURCE_DIR}/docs/"
+        "${CMAKE_CURRENT_SOURCE_DIR}/docs"
         "${SPHINX_HTML_DIR}"
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/docs"
     COMMENT "Building HTML documentation with Sphinx")
