ae0c8af34be5f19ace447fbe00d736eb35a00639
moritzleucke <moritzleucke.privat@gmail.com>
2025-10-13 11:39:29 +0200

    fix website #3

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,9 @@ import sys
 
 # Path to your Sphinx extensions
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '_ext')))
+print(">>> CONF FILE DIR:", os.path.dirname(__file__))
+print(">>> CURRENT WORKING DIR:", os.getcwd())
+print(">>> sys.path:", sys.path[:3])
 
 # Configuration file for the Sphinx documentation builder.
 #
